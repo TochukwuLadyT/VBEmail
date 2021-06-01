8b97d2cb6c323b10219f517df58bed83265210da
Lady T <aniukwutochi@gmail.com>
2021-06-01 15:46:02 +0100

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn VBE.wsgi --log-file -
---
+++ voice/urls.py
@@ -0,0 +1,15 @@
+from django.urls import  path
+from . import views
+
+app_name = 'voice'
+
+urlpatterns = [
+    path('', views.login_view, name="login"),
+    path('compose/', views.compose_view, name="compose"),
+    path('inbox/', views.inbox_view, name="inbox"),
+    path('sent/', views.sent_view, name="sent"),
+    path('trash/', views.trash_view, name="trash"),
+    path('options/', views.options_view, name="options")
+]
+
+
